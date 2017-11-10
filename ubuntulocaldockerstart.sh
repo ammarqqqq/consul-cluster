da440af6560de6d97a4689d37c4eca8648f78aa9
Ubuntu <ubuntu@ip-172-31-21-242.eu-central-1.compute.internal>
2017-11-10 15:35:58 +0000

    -

--- ubuntulocaldockerstart.sh
+++ ubuntulocaldockerstart.sh
@@ -8,5 +8,5 @@ docker-compose down -v --remove-orphans
 #docker-compose up -d --force-recreate --build
 # optional: list machines, images, and containers
 #docker-compose run --rm start_dependencies
-docker-compose  -f consul-cluster1.yml up  --force-recreate --build
+docker-compose  -f consul-cluster2.yml up  --force-recreate --build
 #docker-compose -f docker-compose.yml  -f docker-compose.blockchain.yml up --force-recreate --build
